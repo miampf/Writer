e2f78ef44aa2ae74acc492dcfa0da2b71b563c83
motte <miampf@proton.me>
2026-03-04 18:27:49 +0100

    chore: initial commit

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "A neovim distribution for creative writing";
+
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixvim = {
+        url = "github:nix-community/nixvim";
+        inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
+      perSystem = { config, self', inputs', pkgs, system, ... }: {
+        packages.default = pkgs.writeShellScriptBin "writer" ''
+          exec -a $0 ${inputs.nixvim.legacyPackages.${system}.makeNixvim (import ./config.nix)}/bin/nvim $@
+        '';
+      };
+    };
+}
---
+++ plugins.nix
@@ -0,0 +1,10 @@
+{
+  goyo.enable = true;
+  render-markdown.enable = true;
+  treesitter = {
+    enable = true;
+    highlight.enable = true;
+  };
+  fugitive.enable = true;
+  yazi.enable = true;
+}
